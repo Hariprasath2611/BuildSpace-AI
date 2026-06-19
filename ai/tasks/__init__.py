9b3b4ff5922ef83626b4651bdb7ecf7fa44ebbc6
Hariprasath2611 <hariprasathd26112006@gmail.com>
2026-06-19 10:53:29 +0530

    updated project

---
+++ ai/tasks/__init__.py
@@ -0,0 +1,3 @@
+# Packaged tasks
+from ai.tasks.vision_tasks import process_blueprint_task, check_site_progress_task, detect_safety_hazards_task
+from ai.tasks.ocr_tasks import parse_ocr_document_task
